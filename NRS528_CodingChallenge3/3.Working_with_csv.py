33346bc1a6d21e774e4c798991d08cf0ac06b305
Andrew Davies <davies@uri.edu>
2024-02-22 09:07:41 -0500

    Added feedback

--- NRS528_CodingChallenge3/3.Working_with_csv.py
+++ NRS528_CodingChallenge3/3.Working_with_csv.py
@@ -7,6 +7,8 @@
 # Minimum, maximum and average for the entire dataset.
 # Seasonal average if Spring (March, April, May), Summer (June, July, August), Autumn (September, October, November)
 # and Winter (December, January, February).
+#extract mopnth -- month = row[0].split('-')[1]
+
 # Calculate the anomaly for each value in the dataset relative to the mean for the entire time series.
 
 # import csv file
@@ -14,27 +16,67 @@ import csv
 
 date_list, value_list = [], []
 
-# calculate the average CO2 value for every year in the dataset
+# Establish date list
+with open("co2_ppm_daily.csv") as co2_csv:
+    next(co2_csv)
+    for row in csv.reader(co2_csv):
+        year = row[0].split('-')[0]
+        if year not in date_list:
+            date_list.append(year)
 
-average_co2_per_year = 0
+
 average_co2_per_year_dict = {}
-
-for d in date_list:
+for year in date_list:
     with open("co2_ppm_daily.csv") as co2_csv:
-        for row in csv.reader(co2_csv):
-            if row[0] == d:
-                average_co2_per_year += int(float(row[1]))
 
-    average_co2_per_year_dict[d] = average_co2_per_year
-    average_co2_per_year = 0
+        average_co2_per_year = []
+
+        next(co2_csv)
+        for row in csv.reader(co2_csv):
+            if row[0].split('-')[0] == year:
+                average_co2_per_year.append(float(row[1]))
+
+        average_co2_per_year_dict[year] = sum(average_co2_per_year) / len(average_co2_per_year)
 
 print(average_co2_per_year_dict)
 
+# Min, max, mean
+dataset_values = []
 
+with open("co2_ppm_daily.csv") as co2_csv:
+    next(co2_csv)
+    for row in csv.reader(co2_csv):
+        dataset_values.append(float(row[1]))
 
+dataset_min = min(dataset_values)
+dataset_max = max(dataset_values)
+dataset_mean = sum(dataset_values) / len(dataset_values)
 
+print(dataset_min)
+print(dataset_max)
+print(dataset_mean)
 
-
-
-
-
+# calculate the average CO2 value for every year in the dataset
+#
+# average_co2_per_year = 0
+# average_co2_per_year_dict = {}
+#
+# for d in date_list:
+#     with open("co2_ppm_daily.csv") as co2_csv:
+#         for row in csv.reader(co2_csv):
+#             if row[0] == d:
+#                 average_co2_per_year += int(float(row[1]))
+#
+#     average_co2_per_year_dict[d] = average_co2_per_year
+#     average_co2_per_year = 0
+#
+# print(average_co2_per_year_dict)
+#
+#
+#
+#
+#
+#
+#
+#
+#
