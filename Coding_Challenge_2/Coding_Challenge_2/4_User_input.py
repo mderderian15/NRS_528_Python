8faf5a3daa49527916f84984a507086898071c57
Michaela Derderian <157772065+mderderian15@users.noreply.github.com>
2024-05-04 09:04:33 -0400

    Update 4_User_input.py

--- Coding_Challenge_2/Coding_Challenge_2/4_User_input.py
+++ Coding_Challenge_2/Coding_Challenge_2/4_User_input.py
@@ -7,7 +7,11 @@
 # age = input("What is your age? ")
 # print "Your age is " + str(age)
 
+# Create input for user's age
 age = int(input("What is your age? "))
+
+# Create value for years left until retirement
 retire_in = 65 - age
+
 print ("Your age is " + str(age))
 print("You are " + str(retire_in) + " years away from retirement")
